cb4d60bd516823fe1f7b092ea7c8be2f71c7469c
EC2 Default User <ec2-user@ip-172-31-24-171.us-west-1.compute.internal>
2025-06-27 05:25:27 +0000

    Trigger deploy after adding AWS secrets

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Deployed via GitHub Actions'));
 app.listen(3000, () => console.log('App running on port 3000'));
+// whitespace change
