076a302b59f2603f097e3efffa871e2efd1a7faa
EC2 Default User <ec2-user@ip-172-31-24-171.us-west-1.compute.internal>
2025-06-27 05:15:33 +0000

    Update homepage message to trigger deploy

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('✅ Hello from Kubernetes on EKS!'));
+app.get('/', (req, res) => res.send('Deployed via GitHub Actions'));
 app.listen(3000, () => console.log('App running on port 3000'));
